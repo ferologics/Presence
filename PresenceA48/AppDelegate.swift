889d72118568cab1571ddd3819f819887cce0c7d
ferologics <f.hetes@gmail.com>
2015-10-03 14:46:36 -0700

    Add Parse support

--- PresenceA48/AppDelegate.swift
+++ PresenceA48/AppDelegate.swift
@@ -4,6 +4,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -11,8 +13,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         ESTConfig.setupAppID("presence-a48", andAppToken: "1f15bfbc76eb65f76fc96ffdef4eb7e8")
-
+        
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("wIP2XuICQ0XllXuJyCTccLVtyQvdBcqrKCdQnnOD",
+            clientKey: "If47RIuAnNCHbVrVORfzkOq2yrAdNdTw8XgwYsib")
+        
+        // Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         // Override point for customization after application launch.
         return true
     }
